529613b4d016ddf8780f08b6d7c81092d7231e8d
ngo-nho <ngonho3703@gmail.com>
2025-09-12 20:10:11 +0700

    Add files via upload

---
+++ Bai2.java
@@ -0,0 +1,15 @@
+public class Bai2 {
+    public static void main(String[] args) {
+        String ten = "Ngo Thi Nho";
+        int tuoi = 19;
+        double chieucao = 1.50;
+        char gioitinh = 'N';
+        boolean thichlaptrinh = true;
+        System.out.println("Thong tin ca nhan:");
+        System.out.println("Ho va ten: " + ten);
+        System.out.println("Tuoi: " + tuoi);
+        System.out.println("Chieu cao: " + chieucao);
+        System.out.println("Gioi tinh: " + gioitinh);
+        System.out.println("Thich lap trinh: " + (thichlaptrinh ? "Co" : "Khong"));
+        }
+    }
